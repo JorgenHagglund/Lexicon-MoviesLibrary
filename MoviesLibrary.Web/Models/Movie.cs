c3e24cd2912e63047a46697394110fd1c03ca771
Jörgen Hägglund <jorgen@sirlucifer.mine.nu>
2025-05-16 09:22:01 +0200

    Started designing the Movie modelAdd project files.

---
+++ MoviesLibrary.Web/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesLibrary.Web.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "The movie needs a title")]
+    [Display(Description = "The title of the movie", Prompt = "Localized title")]
+    public string Title { get; set; }
+
+    [Display(Description = "The original title of the movie", Prompt = "Original title")]
+    public string OriginalTitle { get; set; }
+
+    [Display(Description = "An alternate title", Prompt = "Alternate title")]
+    // TODO: Custom validation
+    public string AlternateTitle { get; set; }
+
+    [DataType(DataType.Duration)]
+    public int Runtime { get; set; } 
+}
